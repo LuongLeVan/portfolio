0387d15f48db137aaffd6785478b7cd4f8cbd975
LuongLeVan <levanluong6a8@gmail.com>
2023-05-26 16:46:07 +0700

    recommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'project';
+export class AppComponent implements OnInit {
+  title = 'Profile';
+  isDarkMode:boolean = false;
+  
+  ngOnInit(): void {
+    
+  }
+
+  handleChangeMode(){
+    this.isDarkMode = !this.isDarkMode;
+  }
+
+  handleSmooth(){
+    
+  }
 }
